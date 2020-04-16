caa0f78d8b5ca99b998cfcba3f95e3edbaf0391e
Pavel Gor <pavelgorshkof@gmail.com>
2020-04-16 18:38:29 +0300

    add loader page and scroll top

--- src/App.vue
+++ src/App.vue
@@ -2,13 +2,41 @@
   div#app(class='app')
     toolbar
     router-view(class='app__content')
+    a(class='app__up-scroll' @click="scrollTop" v-if='upScroll')
+      img(src='./assets/upScroll.svg')
 </template>
 
 <script>
 import Toolbar from './components/Toolbar'
 export default {
   name: 'app',
-  components: { Toolbar }
+  mounted () {
+    window.addEventListener('scroll', this.checkBtnUpScroll)
+  },
+  data: () => ({
+    upScroll: false
+  }),
+  components: { Toolbar },
+  methods: {
+    scrollTop () {
+      const d = document.documentElement
+      const top = setInterval(() => {
+        d.scrollTop = d.scrollTop - 20
+        if (d.scrollTop <= 0) {
+          clearInterval(top)
+          this.upScroll = false
+        }
+      }, 0)
+    },
+    checkBtnUpScroll () {
+      const d = document.documentElement
+      if (d.scrollTop > 350) {
+        this.upScroll = true
+      } else {
+        this.upScroll = false
+      }
+    }
+  }
 }
 </script>
 
@@ -19,6 +47,20 @@ export default {
       margin: 0 auto;
       padding: 100px 60px 60px;
       background-color: transparent;
+    }
+    &__up-scroll{
+      right: 58px;
+      bottom: 58px;
+      border-radius: 50%;
+      cursor: pointer;
+      position: fixed;
+      background: #111013;
+      border: 1px solid #FFFFFF;
+      width: 55px;
+      height: 55px;
+      display: flex;
+      align-items: center;
+      justify-content: center;
     }
   }
 </style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,6 +3,10 @@
     filters(:breeds="breeds" :sub-breeds="subBreeds")
     div(class='home__content')
       card-photo(v-for="photo in photos" :photo="photo")
+    div(class="home__loading" v-if='loading')
+      span
+      span
+      span
 </template>
 
 <script>
@@ -45,16 +49,16 @@ export default {
       this.loading = !this.loading
       const count = await this.fetchPhoto(params)
       if (count < 20) {
-        window.onscroll = function () {}
+        window.removeEventListener('scroll', this.checkBottomScroll)
       } else {
-        window.onscroll = this.checkBottomScroll
+        window.addEventListener('scroll', this.checkBottomScroll)
       }
       this.loading = !this.loading
     },
     async checkBottomScroll () {
-      var d = document.documentElement
-      var offset = d.scrollTop + window.innerHeight
-      var height = d.offsetHeight
+      const d = document.documentElement
+      const offset = d.scrollTop + window.innerHeight
+      const height = d.offsetHeight
       if (offset >= height - 290 && !this.loading) {
         await this.loadData(this.$route.params)
       }
@@ -71,6 +75,32 @@ export default {
       width: 100%;
       grid-template-columns: repeat(auto-fill, minmax(290px, 1fr));
       grid-gap: 30px;
+      margin-bottom: 80px;
+    }
+    &__loading{
+      display: flex;
+      align-items: center;
+      justify-content: center;
+      & > span{
+        border: 1px solid #FFFFFF;
+        border-radius: 50%;
+        margin-right: 15px;
+        &:last-of-type{
+          margin-right: 0;
+        }
+        &:nth-child(1){
+          width: 12px;
+          height: 12px;
+        }
+        &:nth-child(2){
+          width: 14px;
+          height: 14px;
+        }
+        &:nth-child(3){
+          width: 16px;
+          height: 16px;
+        }
+      }
     }
   }
 </style>
